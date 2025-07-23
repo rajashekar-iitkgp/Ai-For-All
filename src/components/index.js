2461c580869df2da15060b5db575d61b454e1d2c
rajashekar-iitkgp <banothrajkumar9959@iitkgp.ac.in>
2025-07-23 12:11:14 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as Brand } from './brand/Brand';
 export { default as CTA } from './cta/CTA';
 export { default as Feature } from './feature/Feature';
 export { default as Navbar } from './navbar/Navbar';
+
 // export { default as Hero } from './summarizer/Hero';
 // export { default as Demo } from './summarizer/Demo';
 // export { default as Library } from './library/Library';
